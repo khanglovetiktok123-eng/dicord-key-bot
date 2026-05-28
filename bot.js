804d8264ce5a2ebe783bb344ef2bd19b94e2e70e
khanglovetiktok123-eng <khanglovetiktok123@gmail.com>
2026-05-28 21:20:05 +0700

    Refactor bot.js to use environment variable for TOKEN

--- bot.js
+++ bot.js
@@ -1,13 +1,16 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const {
+    Client,
+    GatewayIntentBits,
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle
+} = require("discord.js");
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds]
 });
 
-const TOKEN ="MTUwOTQ2NDg4NzYyNDA3NzQyMg.GibZ-k.lLV8zfAPG9qzv6CEvVL0wZBlXQ0twDd21Q1r-s";
-
-// API URL
-const API = "https://dicord-key-bot.onrender.com";
+const TOKEN = process.env.TOKEN;
 
 client.once("ready", () => {
     console.log(`${client.user.tag} online`);
@@ -15,71 +18,22 @@ client.once("ready", () => {
 
 client.on("interactionCreate", async interaction => {
 
-    const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
-
-    const row = new ActionRowBuilder().addComponents(
-        new ButtonBuilder()
-            .setLabel("MỞ MENU")
-            .setStyle(ButtonStyle.Link)
-            .setURL("https://raw.githack.com/khanglovetiktok123-eng/dicord-key-bot/main/menu.html")
-    );
-
-    interaction.reply({
-        content: "Menu của bạn đây",
-        components: [row]
-    });
-}", async interaction => {
-
     if (!interaction.isChatInputCommand()) return;
 
-    // createkey
-    if (interaction.commandName === "createkey") {
+    // /menu
+    if (interaction.commandName === "menu") {
 
-        const req = await fetch(API + "/api/create-key", {
-            method: "POST"
-        });
-
-        const data = await req.json();
-
-        interaction.reply(
-            `✅ Key mới: \`${data.key}\``
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setLabel("MỞ MENU")
+                .setStyle(ButtonStyle.Link)
+                .setURL("https://raw.githack.com/khanglovetiktok123-eng/dicord-key-bot/main/menu.html")
         );
-    }
 
-    // checkkey
-    if (interaction.commandName === "checkkey") {
-
-        const key = interaction.options.getString("key");
-
-        const req = await fetch(API + "/api/check-key", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ key })
+        return interaction.reply({
+            content: "🚀 MENU H5GG",
+            components: [row]
         });
-
-        const data = await req.json();
-
-        interaction.reply(data.message);
-    }
-
-    // listkeys
-    if (interaction.commandName === "listkeys") {
-
-        const req = await fetch(API + "/api/keys");
-
-        const keys = await req.json();
-
-        if (keys.length === 0) {
-            return interaction.reply("Không có key");
-        }
-
-        let text = keys.map(k =>
-            `🔑 ${k.key} | Used: ${k.used}`
-        ).join("\n");
-
-        interaction.reply(text);
     }
 
 });
