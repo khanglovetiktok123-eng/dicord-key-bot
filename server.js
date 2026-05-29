000ca181a4297212f8fde9669ff2eedcbec16fff
khanglovetiktok123-eng <khanglovetiktok123@gmail.com>
2026-05-29 11:14:09 +0700

    server.js
    Add path module and serve index.html file

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const fs = require("fs");
 const cors = require("cors");
-
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(__dirname));
 
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
 const FILE = "./keys.json";
 
 function loadKeys() {
